ff68825e02cfaad2214355fe03fbb27f34782391
Moussa <manuscrit974@gmail.com>
2023-03-13 18:13:16 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  useEffect(() => {
-    const btnActiveModal = document.querySelector(".btnActiveModal");
-    const btnOut = document.querySelector(".btnOut");
-    const modalCenter = document.querySelector(".modalCenter");
-
-    btnActiveModal.onclick = () => {
-      modalCenter.style.display = "initial";
-      btnActiveModal.style.display = "none";
-    };
-    btnOut.onclick = () => {
-      btnActiveModal.style.display = "initial";
-      modalCenter.style.display = "none";
-    };
-  });
+  const [activeModal, setActiveModale] = useState(true);
 
   return (
-    <div className="container">
-      {/* Active your modal with button */}
-      <button className="btnActiveModal btn btn-success px-2">My modal</button>
+    <main className="container">
+      <button
+        id="btnActiveModal"
+        className={
+          activeModal ? "btnActiveModal btn btn-secondary px-2 " : "d-none"
+        }
+        onClick={() => setActiveModale(false)}
+      >
+        Click here
+      </button>
 
       {/* display block */}
-      <div className="modalCenter shadow  mb-5  ">
-        <div className="information bg bg-primary ">
+      <div
+        className={activeModal ? "d-none" : "modalCenter shadow  mb-5 d-block"}
+      >
+        <div className="information bg bg-primary-subtle">
           <h2 className="text-uppercase fw-bolder px-5 pt-2 text-light">
             Information
           </h2>
         </div>
-        <div className="divider my-3 "></div>
-        <div className="bloc px-5">
-          <p>
+
+        <div className="bloc px-5 pt-4">
+          <p className="text-muted">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
             dignissimos cumque libero voluptatem voluptates nihil maiores, dolor
             dolores repudiandae! Debitis animi est hic, voluptatum incidunt
             inventore ab qui. Distinctio, sed?
           </p>
           <div className="d-flex justify-content-end ">
-            <button className="btnOut btn btn-primary my-3 ">Closed</button>
+            <button
+              className="btnOut btn btn-secondary my-3 "
+              onClick={() => setActiveModale(true)}
+            >
+              Closed
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
